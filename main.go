cb7eddb4922af3fc654643290707bc6741edcbc4
Mike Jacobi <mikerjacobi@gmail.com>
2016-07-14 12:44:10 +0000

    test deploy

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	fmt.Println("starting server")
 
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("req rcv2\n")
-		fmt.Fprintf(w, "bar2\n")
+		fmt.Printf("req rcv2 world\n")
+		fmt.Fprintf(w, "bar2worldworld\n")
 	})
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+os.Getenv("PORT"), nil))
